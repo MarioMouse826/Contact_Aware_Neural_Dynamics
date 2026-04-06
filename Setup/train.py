d03f17b6fcc4fe919ab223eed0ae10dcda32df17
Montgomery Yu <37722806+MarioMouse826@users.noreply.github.com>
2026-04-06 16:04:26 -0400

    Update comments for clarity in train.py

--- Setup/train.py
+++ Setup/train.py
@@ -5,12 +5,11 @@ print("🌍 Initializing Environment...")
 env = HumanoidLifterEnv()
 
 print("🧠 Initializing SAC Agent...")
-# We use SAC just like you outlined in your proposal
+# Soft-Actor Critic Agent is outlined like in the proposal
 model = SAC("MlpPolicy", env, verbose=1)
 
 print("🚀 Starting Training (This will take a while!)")
-# In your proposal, you set a budget of 500K steps. 
-# Let's start with 10,000 just to make sure it works without crashing.
+# We have a budget of 500K steps total 
 model.learn(total_timesteps=50000, log_interval=1, progress_bar=True)
 
 print("💾 Saving the trained brain...")
